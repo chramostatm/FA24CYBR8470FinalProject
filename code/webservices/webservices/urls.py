5a32874043fe804c9c6486ca1d5642c6ecc4c3b5
chramostatm <tmchramosta@gmail.com>
2024-11-16 17:32:48 -0600

    added router

--- code/webservices/webservices/urls.py
+++ code/webservices/webservices/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from CollectionApp.views import MovieVS
+router = DefaultRouter()
+router.register(r'api/movies', MovieVS)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
