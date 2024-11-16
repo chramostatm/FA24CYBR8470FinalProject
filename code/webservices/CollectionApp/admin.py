23f610c72b67c89482c6e8ddea73e8b4aa779958
chramostatm <tmchramosta@gmail.com>
2024-11-16 17:31:32 -0600

    register Movie model to admin

--- code/webservices/CollectionApp/admin.py
+++ code/webservices/CollectionApp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from .models import Movie
 # Register your models here.
+
+
+class MoveAdmin(admin.ModelAdmin):
+    # Define the list of fields to display in the admin interface
+    list_display = ('name', 'year', 'director', 'mediaFormat'
+                    #owner
+                    )
+    
+    # Add search functionality
+    search_fields = ('name', 'year', 'director', 'mediaFormat'
+                    #owner
+                    )
+    # Add filters
+    list_filter = ('age', 'breed')
+
+    # Define which fields can be clicked to view the details page
+    list_display_links = ('name',)
+
+    # Define how fields are displayed when editing a Dog instance
+    fields = ('name', 'year', 'director', 'mediaFormat'
+                    #owner
+            )
+
+# Register the model and admin class
+admin.site.register(Dog, DogAdmin)
